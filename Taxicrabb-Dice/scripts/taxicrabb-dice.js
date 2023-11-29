7e773da846ae39373ec6de1a982d13621a16f327
taxicrabb <152030282+taxicrabb@users.noreply.github.com>
2023-11-29 12:21:23 -0800

    Update taxicrabb-dice.js

--- Taxicrabb-Dice/scripts/taxicrabb-dice.js
+++ Taxicrabb-Dice/scripts/taxicrabb-dice.js
@@ -6,4 +6,15 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
 	    source: "modules/taxicrabb-dice/textures/Mapleleaftext.webp",
 	    bump: "modules/taxicrabb-dice/textures/Mapleleaftext.webp"
 	});
+
+	  dice3d.addSystem({id:"Rollplayer1",name:"⧋ Rollplayer1 (d20, 20 best)"},false);
+
+  dice3d.addDicePreset({
+    type:"d20",
+    labels:[
+      'No','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
+      'modules/taxicrabb-dice/faces/RP1.webp'
+    ],
+    system:"Rollplayer1"
+  });
 });
