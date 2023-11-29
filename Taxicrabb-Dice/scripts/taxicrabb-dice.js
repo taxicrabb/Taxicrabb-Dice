f860434569702445058dcc0feb7b63c8c7a8e22e
taxicrabb <152030282+taxicrabb@users.noreply.github.com>
2023-11-29 13:27:38 -0800

    Update taxicrabb-dice.js

--- Taxicrabb-Dice/scripts/taxicrabb-dice.js
+++ Taxicrabb-Dice/scripts/taxicrabb-dice.js
@@ -12,7 +12,7 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
   dice3d.addDicePreset({
     type:"d20",
     labels:[
-      'No','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
+      '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
       'modules/taxicrabb-dice/faces/RollP1.webp'
     ],
     system:"Rollplayer1"
@@ -27,6 +27,29 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
       'UwU'
     ],
     system:"uwu"
+  });
+
+  dice3d.addSystem({id:"Wolf",name:"⧋ Werewolf (d20)"},false);
+
+  dice3d.addDicePreset({
+    type:"d20",
+    labels:[
+      'modules/taxicrabb-dice/faces/wolfd1.webp','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
+      'modules/taxicrabb-dice/faces/wolfd20.webp'
+    ],
+    system:"Wolf"
+  });
+
+
+  dice3d.addSystem({id:"Goat",name:"⧋ Goat (d20)"},false);
+
+  dice3d.addDicePreset({
+    type:"d20",
+    labels:[
+      'Bah','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
+      'modules/taxicrabb-dice/faces/goatd20.webp'
+    ],
+    system:"Goat"
   });
 	
 });
